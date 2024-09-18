00b63a7a785e6f160e546939b63e7be03ca35ef4
luis10ibarra <52299032+luis10ibarra@users.noreply.github.com>
2024-09-18 11:04:02 -0500

    Add files via upload

---
+++ ercot-lmp-fuzzymatch.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 18 11:00:35 2024
+
+@author: Luis.Ibarra
+"""
+
+import pandas as pd 
+
+file1 = 'Settlement_Points_09062024_134422.csv'
+file2 = 'NOIE_Mapping_09062024_134422.csv' 
+file3 = 'MORA_November20241.xlsx' 
+
+sp = pd.read_csv(file1) 
+noie = pd.read_csv(file2) 
+mora = pd.read_excel(file1)
